25abded1321ade48e92a7b59c5548d0d92664d73
RafaelTrust <31193127+RafaelTrust@users.noreply.github.com>
2024-03-12 09:28:39 -0300

    Tentando arrumar erro

--- Assets/ScriptsCopy/ListaPerguntas.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ListaPerguntas : MonoBehaviour
-{
-    public List<Perguntas> listaPerguntas = new List<Perguntas>();
-}
--- Assets/ScriptsCopy/LoginResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LoginResponse
-{
-    public string token;
-    public string nick;
-}
